8117bbbbb6125b2aa573b83998133d869f697840
Doug Metz <dwmetz@gmail.com>
2022-07-18 12:39:54 -0400

    Scripts updated

---
+++ AX-Collect.ps1
@@ -0,0 +1,14 @@
+<# AX-Collect.ps1
+https://github.com/dwmetz/Axiom-PowerShell/AX-Collect.ps1
+Author: @dwmetz
+Function: This script enables investigator access to a users O365 mailbox.
+#>
+
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+$script:axiom_o365 = Read-Host -Prompt 'Enter the used ID of the target (ex.R12345)'
+$script:axiom_examiner =Read-Host -Prompt 'Enter the used ID of the examiner (ex.R34567)'
+Add-MailboxPermission -Identity "$script:axiom_o365" -User "$script:axiom_examiner" -AccessRights FullAccess
+Write-Host -ForegroundColor Green $script:axiom_examiner 'permissions added to' $script:axiom_o365 'mailbox.'
---
+++ AX-YaraSync.ps1
@@ -0,0 +1,23 @@
+﻿<# AX-YaraSync.ps1
+https://github.com/dwmetz/Axiom-PowerShell/AX-YaraSync.ps1
+Author: @dwmetz
+Function:
+    Script to sync YARA updates from Reversing Labs Github, archive the existing YARA rules in Axiom, and update Axiom with the new rules.
+
+#>
+# Set the working directory to the local git repository for the YARA rules
+Set-Location  C:\GitHub\reversinglabs-yara-rules\
+# sync the repository; requires github CLI https://cli.github.com/
+gh repo sync
+# Create local archive directory
+mkdir C:\Archives -Force
+# Backup the existing YARA rules in Axiom
+Get-ChildItem -Path "C:\Program Files\Magnet Forensics\Magnet AXIOM\YARA" | Compress-Archive -DestinationPath C:\Archives\AxiomYARA.zip
+# Variable for date/time
+$timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
+# Set the working directory to the Archives location
+Set-Location "C:\Archives"
+# Rename the archive with timestamp
+Get-ChildItem -Filter '*AxiomYARA*' -Recurse | Rename-Item -NewName {$_.name -replace 'AxiomYARA', $timestamp }
+# Copy new YARA rules to Axiom
+robocopy /s C:\GitHub\reversinglabs-yara-rules\yara "C:\Program Files\Magnet Forensics\Magnet AXIOM\YARA\ReversingLabs"
