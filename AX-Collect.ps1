96fbbdcebfe67544726f5a035d97d91df346bf5e
Doug Metz <dwmetz@gmail.com>
2022-10-25 14:20:13 -0400

    Update AX-Collect.ps1

--- AX-Collect.ps1
+++ AX-Collect.ps1
@@ -1,14 +1,18 @@
-<# AX-Collect.ps1
-https://github.com/dwmetz/Axiom-PowerShell/blob/main/AX-Collect.ps1
+<# AxCollect.ps1 
+version 2.0
+https://github.com/dwmetz/Axiom-PowerShell
 Author: @dwmetz
-Function: This script enables investigator access to a users O365 mailbox.
-#>
+Function:
+    This script enables investigator access to a users O365 mailbox.
+    Script shoud be run by Exchange Administrator to grant mailbox delegation permissions.
+    Once enabled, sign in using investigator credentials to perform the cloud collection in Axiom. 
 
-Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-$script:axiom_o365 = Read-Host -Prompt 'Enter the used ID of the target (ex.R12345)'
-$script:axiom_examiner =Read-Host -Prompt 'Enter the used ID of the examiner (ex.R34567)'
-Add-MailboxPermission -Identity "$script:axiom_o365" -User "$script:axiom_examiner" -AccessRights FullAccess
+25.October.2022 - update ExchangeOnlineManagement connection
+
+#>#>
+Import-module ExchangeOnlineManagement
+Connect-ExchangeOnline
+$script:axiom_o365 = Read-Host -Prompt 'Enter the used ID of the target (ex.Alice)'
+$script:axiom_examiner =Read-Host -Prompt 'Enter the used ID of the examiner (ex.Bob)'
+Add-MailboxPermission -Identity "$script:axiom_o365" -User "$script:axiom_examiner" -AccessRights FullAccess -ErrorAction stop
 Write-Host -ForegroundColor Green $script:axiom_examiner 'permissions added to' $script:axiom_o365 'mailbox.'
